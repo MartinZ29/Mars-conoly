66ab2e01fdf67518a55e27a80853e009ef3cfb0c
martinz <martinzhangyx@gmail.com>
2016-11-15 13:34:43 -0800

    keep on

--- src/app/register/register.component.ts
+++ src/app/register/register.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Colonist } from '../models';
 
 @Component({
   selector: 'app-register',
@@ -6,6 +7,8 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./register.component.css']
 })
 export class RegisterComponent implements OnInit {
+
+  colonist: Colonist;
 
   constructor() { }
 
