226aa0cb42c3e82af5932a2ff672c81075d4c03f
martinz <martinzhangyx@gmail.com>
2016-11-15 14:40:12 -0800

    form json

--- src/app/services/jobs.service.ts
+++ src/app/services/jobs.service.ts
@@ -1,8 +1,24 @@
 import { Injectable } from '@angular/core';
+import { Http, Response } from '@angular/http';
+import { Observable } from 'rxjs/Observable';
+import { Job} from '../models';
 
 @Injectable()
 export class JobsService {
 
-  constructor() { }
+
+JOBS_JSON = 'https://red-wdp-api.herokuapp.com/api/mars/jobs';
+
+  constructor( private http: Http ) { }
+
+  getJobs(): Observable<Response>  {
+    return this.http
+               .get(this.JOBS_JSON)
+               .map((res: Response) => {
+                      let body = res.json();
+                      return body.data;
+                    });
+                    
+  }
 
 }
