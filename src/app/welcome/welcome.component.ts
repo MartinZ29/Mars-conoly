2b093245b6d89a12f7d15889e218f45310035524
martinz <martinzhangyx@gmail.com>
2016-11-18 15:06:04 -0800

    hint for anmation

--- src/app/welcome/welcome.component.ts
+++ src/app/welcome/welcome.component.ts
@@ -1,11 +1,18 @@
 import { Component, OnInit } from '@angular/core';
 
+// import trigger,animate.. first
+
 @Component({
   selector: 'app-welcome',
   templateUrl: './welcome.component.html',
   styleUrls: ['./welcome.component.css']
+  // animateions[] add here !!
+
 })
 export class WelcomeComponent implements OnInit {
+
+// add @Hostbinding  return true;
+
 
   constructor() { }
 
