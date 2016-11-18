ccd481df2d5120033c5bae014db4e2fbd585fba3
martinz <martinzhangyx@gmail.com>
2016-11-18 11:34:42 -0800

    add submitcolonist function

--- src/app/services/colonists.service.ts
+++ src/app/services/colonists.service.ts
@@ -1,8 +1,23 @@
 import { Injectable } from '@angular/core';
+import { Http, Response, Headers} from '@angular/http';
+import { Observable } from 'rxjs/Observable';
+import { Colonist, NewColonist } from '../models';
+import 'rxjs/add/operator/map';
 
 @Injectable()
-export class ColonistsService {
+export default class ColonistsService {
 
-  constructor() { }
+
+COLONIST_JSON = 'https://red-wdp-api.herokuapp.com/api/mars/colonists';
+
+  constructor( private http: Http) { }
+
+  submitColonist(colonist: NewColonist): Observable<Colonist> {
+      const headers = new Headers();
+      headers.append('Content-Type', 'application/json');
+
+      return this.http.post(this.COLONIST_JSON, {colonist}, {headers})
+                      .map((res: Response) => res.json().colonist);
+  }
 
 }
