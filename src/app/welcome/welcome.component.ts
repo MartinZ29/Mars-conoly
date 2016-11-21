50953ee5b9f744f788950dcba5cd0b7293947cd0
martinz <martinzhangyx@gmail.com>
2016-11-20 23:04:53 -0800

    add animation

--- src/app/welcome/welcome.component.ts
+++ src/app/welcome/welcome.component.ts
@@ -1,12 +1,38 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, HostBinding, trigger, transition, animate, style, state } from '@angular/core';
 
 // import trigger,animate.. first
 
 @Component({
   selector: 'app-welcome',
   templateUrl: './welcome.component.html',
-  styleUrls: ['./welcome.component.scss']
+  styleUrls: ['./welcome.component.scss'],
   // animateions[] add here !!
+  animations: [
+    trigger('routeAnimation', [
+      state('*',
+      style({
+        width:100%,
+        opacity:1,
+        transform:'translateX(0)'
+      })),
+      transition('void => *', [
+        style({
+          width:100%,
+          opacity:0,
+          transform:'translateX(-100%)'
+        }),
+        animate('1s ease-out')
+      ]),
+      transition('* => void', [
+        animate('1s ease-in', style({
+          width:100%,
+          opacity:0,
+          transform:'translateX(100%)'
+        }))
+      ])
+    ])
+  ]
+
 
 })
 export class WelcomeComponent implements OnInit {
